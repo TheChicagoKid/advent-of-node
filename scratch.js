2be56671bfe3ebc8d505cea604b221df925cf801
Ty Rodriguez <ty.rodriguez2021@gmail.com>
2023-12-01 19:11:11 -0600

    create scratch file to try part 2

--- scratch.js
+++ scratch.js
@@ -1,9 +1,42 @@
-const s = 'a5sjgl'
+words = [
+  "two1nine",
+  "eightwothree",
+  "abcone2threexyz",
+  "xtwone3four",
+  "4nineeightseven2",
+  "zoneight234",
+];
 
-const digits = '1234567890'
+function check(str, nums) {
 
-for (str of s) {
-    if (digits.includes(str)) {
-        console.log(str)
+  for (num of nums) {
+    if (num.startsWith(str)) {
+      return true
     }
+  }
+
+  return false
+}
+
+const digits = "1234567890";
+
+for (word of words) {
+  nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+  left = 0
+  right = 0
+
+  for (s of word) {
+
+    slice = s.slice(left, right)
+
+    while (check(slice, nums)) {
+      if (nums.includes(slice)) {
+        console.log(slice)
+        contiune
+      }
+      right++
+    }
+    l
+  }
 }
